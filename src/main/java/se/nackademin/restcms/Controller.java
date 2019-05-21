10388ec5082bb33863bab6a533905e5c6a41f365
nikalsh <niklas@helander.se>
2019-05-21 18:34:24 +0200

    added restcontroller with helloworld

---
+++ src/main/java/se/nackademin/restcms/Controller.java
@@ -0,0 +1,15 @@
+package se.nackademin.restcms;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String HelloWorld() {
+        return "HELLO WORLD";
+    }
+
+}
