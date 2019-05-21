1cc21f98373435afc9745aaedc0e1fe30cf56bbe
nikalsh <niklas@helander.se>
2019-05-21 18:16:58 +0200

    test

---
+++ src/main/java/se/nackademin/restcms/RestcmsApplication.java
@@ -0,0 +1,13 @@
+package se.nackademin.restcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestcmsApplication.class, args);
+    }
+
+}
