02e4dfded708645b67fc945124acfc72eb3f5dba
Josef <Josef.NymanMuller@yh.nackademin.se>
2019-05-30 12:57:38 +0200

    Merge remote-tracking branch 'origin/ master'

---
+++ src/main/java/se/nackademin/restcms/crudrepositories/ImageFileRepository.java
@@ -0,0 +1,11 @@
+package se.nackademin.restcms.crudrepositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import se.nackademin.restcms.entities.ImageFile;
+
+@RepositoryRestResource(exported = false)
+public interface ImageFileRepository extends JpaRepository<ImageFile, String> {
+
+
+}
